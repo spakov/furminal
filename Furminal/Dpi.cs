739fd400c776548eca9d85c975c042fba47439d8
spakov <3934369+spakov@users.noreply.github.com>
2025-07-27 10:37:04 -0600

    Implemented DPI awareness
    - Fixes window being wrongly sized for terminal

---
+++ Furminal/Dpi.cs
@@ -0,0 +1,11 @@
+﻿namespace Spakov.Furminal
+{
+    /// <summary>
+    /// A window's DPI.
+    /// </summary>
+    internal struct Dpi
+    {
+        public uint X;
+        public uint Y;
+    }
+}
